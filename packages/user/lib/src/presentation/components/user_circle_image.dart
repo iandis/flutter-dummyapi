ae987f7bf2a1aac9a0617ca7e455f5ed1a230818
Iandi Santulus <iandisantulusn@gmail.com>
2022-03-16 14:15:33 +0700

    feat(user): add `UserCircleImage` widget

---
+++ packages/user/lib/src/presentation/components/user_circle_image.dart
@@ -0,0 +1,34 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import 'user_image_placeholder.dart';
+
+class UserCircleImage extends StatelessWidget {
+  const UserCircleImage({
+    Key? key,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      minRadius: 20,
+      maxRadius: 21,
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+        child: CachedNetworkImage(
+          fit: BoxFit.cover,
+          height: 40,
+          width: 40,
+          imageUrl: imageUrl,
+          placeholder: (_, __) => const UserImagePlaceholder(),
+          errorWidget: (_, __, ___) {
+            return const UserImagePlaceholder();
+          },
+        ),
+      ),
+    );
+  }
+}
