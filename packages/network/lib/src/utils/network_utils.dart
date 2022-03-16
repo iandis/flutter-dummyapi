1b9f8664106f1ad592b8bf3354d576e88b5d2cfb
Iandi Santulus <iandisantulusn@gmail.com>
2022-03-16 12:53:47 +0700

    refactor(network): remove unused constructor on `NetworkUtils`

--- packages/network/lib/src/utils/network_utils.dart
+++ packages/network/lib/src/utils/network_utils.dart
@@ -1,8 +1,6 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 
 abstract class NetworkUtils {
-  const NetworkUtils._();
-
   static Future<bool> get isConnected async {
     final ConnectivityResult result = await Connectivity().checkConnectivity();
 
